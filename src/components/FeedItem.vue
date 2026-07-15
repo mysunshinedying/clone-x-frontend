a1e1e795506455eb439b5ad141f78860677d401d
bize <bizecommission@gmail.com>
2026-07-15 20:56:47 +0900

    [feat] 피드 초기 데이터 입력

---
+++ src/components/FeedItem.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+  name: "FeedItem",
+  props: {
+    feed: {
+      type: Object,
+      required: true,
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="feed-container">
+    <div class="feed-header">
+      <div class="feed-content">
+        {{ feed.content }}
+      </div>
+      <button class="feed-delete-button">X</button>
+    </div>
+    <div class="feed-name">{{ feed.user.name }}</div>
+  </div>
+</template>
+
+<style scoped>
+
+.feed-container {
+  height: 80px;
+  background-color: white;
+  margin: 10px 0;
+  color: black;
+  padding: 3%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 10px;
+}
+
+.feed-header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.feed-content {
+  padding: 1%;
+}
+
+.feed-delete-button {
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
+.feed-name {
+  text-align: right;
+  font-size: 12px;
+}
+
+</style>
--- src/components/FeedList.vue
+++ src/components/FeedList.vue
@@ -1,62 +1,110 @@
 <script>
+import FeedItem from "@/components/FeedItem.vue";
+
+const testData = [
+  {
+    id: 11,
+    content: "코딩은 재밌어!",
+    user: {
+      id: 12,
+      name: "테스터"
+    }
+  },
+  {
+    id: 10,
+    content: "JS 디버깅 중…",
+    user: {
+      id: 12,
+      name: "테스터"
+    }
+  },
+  {
+    id: 9,
+    content: "커피 한 잔의 여유...",
+    user: {
+      id: 13,
+      name: "다미장"
+    }
+  },
+  {
+    id: 6,
+    content: "주말에 드라마 몰아보기",
+    user: {
+      id: 13,
+      name: "다미장"
+    }
+  },
+  {
+    id: 5,
+    content: "우리 집 강아지 너무 귀여워",
+    user: {
+      id: 13,
+      name: "다미장"
+    }
+  },
+  {
+    id: 4,
+    content: "2025년에는 꼭 운동한다!",
+    user: {
+      id: 11,
+      name: "user"
+    }
+  },
+  {
+    id: 3,
+    content: "디버깅만 몇 시간째…",
+    user: {
+      id: 11,
+      name: "user"
+    }
+  },
+  {
+    id: 2,
+    content: "아침 커피로 하루 시작",
+    user: {
+      id: 11,
+      name: "user"
+    }
+  },
+  {
+    id: 1,
+    content: "오늘도 멋진 하루 되세요!",
+    user: {
+      id: 11,
+      name: "user"
+    }
+  }
+]
+
 export default {
   name: "FeedList",
-  props: {
-    feed: {
-      type: Array,
-      required: true,
+  data() {
+    return {
+      initFeedData: []
     }
+  },
+  components: {FeedItem},
+  created() {
+    this.initFeedData = testData
   },
 }
 </script>
 
 <template>
   <div class="feed-list-container">
-    <div class="feed-container" v-for="item in feed" :key="item.id">
-      <div class="feed-header">
-        <div class="feed-content">
-          {{ item.content }}
-        </div>
-        <button class="feed-delete-button">X</button>
-      </div>
-      <div class="feed-name">{{ item.user.name }}</div>
+    <div v-if="initFeedData.length > 0">
+      <feedItem v-for="feed in initFeedData" :key="feed.id" :feed="feed"/>
+    </div>
+    <div v-else>
+      <div>피드가 없습니다.</div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.feed-container {
-  height: 80px;
-  background-color: white;
-  margin: 10px 0;
-  color: black;
-  padding: 3%;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  border-radius: 10px;
-}
-
-.feed-header {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.feed-content {
-  padding: 1%;
-}
-
-.feed-delete-button {
-  background: none;
-  border: none;
-  cursor: pointer;
-}
-
-.feed-name {
-  text-align: right;
-  font-size: 12px;
+.feed-list-container {
+  height: 60vh;
+  overflow-y: auto;
 }
 
 </style>
